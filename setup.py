35ca5ee4dcb583327e6c59c8e05b6cca69404a8f
Kien Pham <kien@sendgrid.com>
2013-04-16 11:48:56 -0700

    includes the code for real now

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='airbrake-flask',
-    version='0.0.1',
+    version='0.0.2',
     author='Kien Pham',
     author_email='kien@sendgrid.com',
-    packages=find_packages(),
+    py_modules=['airbrake'],
     url='https://github.com/kienpham2000/airbrake-flask',
     license='LICENSE.txt',
     description='Airbrake client for Python Flask',
