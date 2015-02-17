3f2016f220e8d68ddd01ec3b551e7301c64fd4ab
deathowl <csergo.balint@ustream.tv>
2015-02-17 13:31:44 +0100

    let it be six.

--- airbrake/airbrake.py
+++ airbrake/airbrake.py
@@ -3,6 +3,7 @@ import traceback
 import requests
 import os
 import sys
+from six import text_type
 from xml.etree.ElementTree import Element, tostring, SubElement
 from . import __app_name__, __version__, __app_url__
 
@@ -54,11 +55,6 @@ class AirbrakeErrorHandler(logging.Handler):
 
         return (status.status_code, status.text)
 
-    def format_data(self, data):
-        if sys.version_info[0] > 2:
-            return str(data)
-        else:
-            return unicode(data)
 
 
 
@@ -89,42 +85,42 @@ class AirbrakeErrorHandler(logging.Handler):
         if self.session:
             request_session = SubElement(request_xml, 'params')
             for key in self.session:
-                SubElement(request_session, 'var', dict(key=key)).text = (self.format_data(
+                SubElement(request_session, 'var', dict(key=key)).text = (text_type(
                     self.session[key]))
 
         # check for form, args, body or json data
         params = SubElement(request_xml, 'params')
         if self.request['values']:
             for key in self.request['values']:
-                SubElement(params, 'var', dict(key=key)).text = (self.format_data(
+                SubElement(params, 'var', dict(key=key)).text = (text_type(
                     self.request['values'].get(key)))
 
         if self.request['body']:
-            SubElement(params, 'var', dict(key='body')).text = (self.format_data(
+            SubElement(params, 'var', dict(key='body')).text = (text_type(
                 self.request['body']))
 
         if self.request.get('json'):
             for key in self.request['json']:
-                SubElement(params, 'var', dict(key=key)).text = (self.format_data(
+                SubElement(params, 'var', dict(key=key)).text = (text_type(
                     self.request['json'].get(key)))
 
         # TODO: add self.meta_variables here, learn how to extract from Flask
         cgi_data = SubElement(request_xml, 'cgi-data')
         for key, value in os.environ.items():
             if key in self.env_variables:
-                SubElement(cgi_data, 'var', dict(key=key)).text = self.format_data(value)
+                SubElement(cgi_data, 'var', dict(key=key)).text = text_type(value)
         if self.request['remote_addr']:
-            SubElement(cgi_data, 'var', dict(key='remote_addr')).text = self.format_data(
+            SubElement(cgi_data, 'var', dict(key='remote_addr')).text = text_type(
                 self.request['remote_addr'])
 
         if self.request['headers']:
             for key, value in self.request['headers']:
-                SubElement(cgi_data, 'var', dict(key=key)).text = self.format_data(value)
+                SubElement(cgi_data, 'var', dict(key=key)).text = text_type(value)
 
         # setting class name
         error = SubElement(xml, 'error')
         SubElement(error, 'class').text = exception.__class__.__name__
-        SubElement(error, 'message').text = self.format_data(exception)
+        SubElement(error, 'message').text = text_type(exception)
 
         # setting backtrace line
         backtrace = SubElement(error, 'backtrace')
@@ -132,7 +128,7 @@ class AirbrakeErrorHandler(logging.Handler):
             for (pathname, lineno, func_name,
                  text) in traceback.extract_tb(trace)[::-1]:
                 SubElement(backtrace, 'line', dict(file=pathname,
-                                                   number=self.format_data(lineno),
+                                                   number=text_type(lineno),
                                                    method='%s: %s' % (func_name,
                                                                       text)))
 
